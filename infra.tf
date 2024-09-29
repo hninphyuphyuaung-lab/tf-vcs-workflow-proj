85cafd3253c83e67fe11ad2a791034eadc0f015a
hninphyuphyuaung-lab <hninphyuphyuaung11@gmail.com>
2024-09-29 11:13:27 +0800

    Modified Infra.tf

--- infra.tf
+++ infra.tf
@@ -1,5 +1,7 @@
-variable "aws_zone" {
-  type        = string
-  description = "AWS zone used for all resources"
-  default     = "us-east-1b"
+resource "aws_vpc" "rancher_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.prefix}-rancher-vpc"
+  }
 }
