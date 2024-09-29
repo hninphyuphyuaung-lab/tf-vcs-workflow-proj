af0f1e3a59f6b3d05de630d89af1180ca29c6472
hninphyuphyuaung-lab <hninphyuphyuaung11@gmail.com>
2024-09-29 11:24:36 +0800

    Added VPC3

--- infra.tf
+++ infra.tf
@@ -7,10 +7,18 @@ resource "aws_vpc" "rancher_vpc" {
 }
 
 resource "aws_vpc" "rancher_vpc2" {
-  cidr_block           = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/32"
   enable_dns_hostnames = true
   tags = {
     Name = "${var.prefix}-rancher-vpc2"
   }
 }
 
+
+resource "aws_vpc" "rancher_vpc3" {
+  cidr_block           = "10.0.0.0/48"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.prefix}-rancher-vpc3"
+  }
+}
